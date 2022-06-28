1f4f1f61bbb3024458e8bd2ffbc4cf0e61e91ef0
VinceBaylon <vincentbaylon@gmail.com>
2022-06-28 05:49:57 -0700

    Initial commit

---
+++ screens/Home.js
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import { View, SafeAreaView, FlatList, Text } from 'react-native'
+
+import { COLORS, NFTData } from '../constants'
+import { HomeHeader, NFTCard, FocusedStatusBar } from '../components'
+
+const Home = () => {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <FocusedStatusBar background={COLORS.primary} />
+
+      <View style={{ flex: 1 }}>
+        <View style={{ zIndex: 0 }}>
+          <FlatList
+            data={NFTData}
+            renderItem={({ item }) => <Text>{item.name}</Text>}
+            keyExtractor={(item) => item.id}
+            showsVerticalScrollIndicator={false}
+            ListHeaderComponent={<HomeHeader />}
+          ></FlatList>
+        </View>
+
+        <View
+          style={{
+            position: 'absolute',
+            top: 0,
+            bottom: 0,
+            right: 0,
+            left: 0,
+            zIndex: -1,
+          }}
+        >
+          <View style={{ height: 300, background: COLORS.primary }} />
+          <View style={{ flex: 1, background: COLORS.white }} />
+        </View>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default Home
